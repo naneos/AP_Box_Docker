113114e10d3a5efd7d44c7356b0734ec6705f3ee
Pelegorn <m.huegi@outlook.com>
2020-06-23 17:48:28 +0200

    implemented some error handlers

--- main.py
+++ main.py
@@ -41,36 +41,29 @@ def write_P2_data_to_influx(P2_data, timestamp):
                 }
             ]
     message_result = client.write_points(datapoints)
-    print(message_result)
+    print("InfluxDB Write success: ",message_result)
 
 def P2_string2data(string_P2):
     try:
         splitedValues = re.split(r'\t[ ]*',string_P2)
-        val = []
+        if len(splitedValues) != 21:
+            print("ERROR: Wrong amount of values in serial data")
+            return False
 
-        val[0] = float(splitedValues[0])
-        val[1] = int(splitedValues[1])
-        val[2] = int(splitedValues[2])
-        val[3] = float(splitedValues[3])
-        val[4] = float(splitedValues[4])
-        val[5] = float(splitedValues[5])
-        val[6] = float(splitedValues[6])
-        val[7] = float(splitedValues[7])
-        val[8] = float(splitedValues[8])
-        val[9] = int(splitedValues[9])
-        val[10] = float(splitedValues[10])
-        val[11] = float(splitedValues[11])
-        val[12] = int(splitedValues[12])
-        val[13] = float(splitedValues[13])
-        val[14] = int(splitedValues[14])
-        val[15] = float(splitedValues[15])
-        val[16] = float(splitedValues[16])
-        val[17] = float(splitedValues[17])
-        val[18] = float(splitedValues[18])
-        val[19] = int(splitedValues[19])
+        val = [ float(splitedValues[0]),    int(splitedValues[1]),
+                int(splitedValues[2]),      float(splitedValues[3]),
+                float(splitedValues[4]),    float(splitedValues[5]),
+                float(splitedValues[6]),    float(splitedValues[7]),
+                float(splitedValues[8]),    int(splitedValues[9]),
+                float(splitedValues[10]),   float(splitedValues[11]),
+                int(splitedValues[12]),     float(splitedValues[13]),
+                int(splitedValues[14]),     float(splitedValues[15]),
+                float(splitedValues[16]),   float(splitedValues[17]),
+                float(splitedValues[18]),   int(splitedValues[19])]
         return val
     except:
         print("ERROR: Serial data corrupted")
+        return False
 
 serQueue = queue.Queue(100)
 
@@ -103,6 +96,7 @@ client = InfluxDBClient('naneosbox', 8086)
 client.create_database(dbname)
 client.switch_database(dbname)
 
+# serPartector = openPort('COM3', 9600)
 serPartector = openPort('/dev/ttyACM0', 9600) #  /dev/ttyACM0 opens the device (P2) on the choosen Port
 threading.Thread(target=serialReadPartector, args=(serPartector,), ).start() # starts the Serial Thread
 line = serQueue.get(True) # throw first value away -> because it is sometimes incomplete
@@ -114,7 +108,8 @@ try:
         timestamp=datetime.datetime.utcnow().isoformat()
         
         P2_data = P2_string2data(line)
-        write_P2_data_to_influx(P2_data, timestamp)
+        if P2_data != False:
+            write_P2_data_to_influx(P2_data, timestamp)
         
         
 
