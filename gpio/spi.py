46a9bba2b6fc16f4eec8e128ba871b4be2d2da36
Pelegorn <m.huegi@outlook.com>
2020-06-25 17:53:12 +0200

    test spi

--- gpio/spi.py
+++ gpio/spi.py
@@ -4,7 +4,7 @@ spi = spidev.SpiDev()
 spi.open(0,0) # opens CS0
 
 temp_bytes = spi.readbytes(4)
-temp = int.from_bytes(temp_bytes)
+temp = int.from_bytes(temp_bytes, byteorder='big')
 
 spi.close
 
