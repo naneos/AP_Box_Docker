f73f834e150e2ecf131dfb88ba08d4df96b97865
Pelegorn <m.huegi@outlook.com>
2020-06-25 18:38:24 +0200

    updated code to function for testing in nodered

--- gpio/spi.py
+++ gpio/spi.py
@@ -1,21 +1,24 @@
 import spidev
 
-spi = spidev.SpiDev()
-spi.open(0,0) # opens CS0
+def readTemp():
+    spi = spidev.SpiDev()
+    spi.open(0,0) # opens CS0
 
-temp_bytes = spi.readbytes(4)
+    temp_bytes = spi.readbytes(4)
 
-temp = int.from_bytes(temp_bytes, byteorder='little')
-error_open_connection = bool(temp & 0x01)
-error_short_circuit_GND = bool((temp >> 1) & 0x01)
-error_short_circuit_VCC = bool((temp >> 2) & 0x01)
-temp_internal = (((temp >> 4) & 0x7FF) * 0.0625) * ((-1) * ((temp >> 15) & 0x01))
-temp_thermocouple = (((temp >> 18) & 0x3FFF) * 0.25) * ((-1) * ((temp >> 31) & 0x01))
+    temp = int.from_bytes(temp_bytes, byteorder='little')
+    error_open_connection = bool(temp & 0x01)
+    error_short_circuit_GND = bool((temp >> 1) & 0x01)
+    error_short_circuit_VCC = bool((temp >> 2) & 0x01)
+    temp_internal = (((temp >> 4) & 0x7FF) * 0.0625) * ((-1) * ((temp >> 15) & 0x01))
+    temp_thermocouple = (((temp >> 18) & 0x3FFF) * 0.25) * ((-1) * ((temp >> 31) & 0x01))
 
-spi.close
+    spi.close
 
-print("Error open connection: " + str(error_open_connection))
-print("Error short circuit to GND: " + str(error_short_circuit_GND))
-print("Error short circuit to VCC: " + str(error_short_circuit_VCC))
-print("Internal junction temperature: " + str(temp_internal) + " °C")
-print("Thermocouple temperature: " + str(temp_thermocouple) + " °C")
+    print("Error open connection: " + str(error_open_connection))
+    print("Error short circuit to GND: " + str(error_short_circuit_GND))
+    print("Error short circuit to VCC: " + str(error_short_circuit_VCC))
+    print("Internal junction temperature: " + str(temp_internal) + " °C")
+    print("Thermocouple temperature: " + str(temp_thermocouple) + " °C")
+
+    return(temp_thermocouple)
