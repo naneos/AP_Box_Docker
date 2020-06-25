dc49c2c872cb712689ccc07d18f3155adf2157fe
Pelegorn <m.huegi@outlook.com>
2020-06-25 18:30:25 +0200

    corrected wrong byteroder added internal junction temp

--- gpio/spi.py
+++ gpio/spi.py
@@ -5,13 +5,15 @@ spi.open(0,0) # opens CS0
 
 temp_bytes = spi.readbytes(4)
 
-temp = int.from_bytes(temp_bytes, byteorder='big')
+temp = int.from_bytes(temp_bytes, byteorder='little')
 error_open_connection = bool(temp & 0x01)
 error_short_circuit_GND = bool((temp >> 1) & 0x01)
 error_short_circuit_VCC = bool((temp >> 2) & 0x01)
+temp_internal = (((temp >> 4) & 0x7FF) * 0.0625) * ((-1) * ((temp >> 15) & 0x01))
 
 spi.close
 
 print("Error open connection: " + str(error_open_connection))
 print("Error short circuit to GND: " + str(error_short_circuit_GND))
 print("Error short circuit to VCC: " + str(error_short_circuit_VCC))
+print("Internal junction temperature: " + str(temp_internal) + " °C")
