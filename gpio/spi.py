9c7d03fd6e89560643642d693683fe74991891aa
Pelegorn <m.huegi@outlook.com>
2020-06-25 18:33:35 +0200

    added thermocouple temp

--- gpio/spi.py
+++ gpio/spi.py
@@ -10,6 +10,7 @@ error_open_connection = bool(temp & 0x01)
 error_short_circuit_GND = bool((temp >> 1) & 0x01)
 error_short_circuit_VCC = bool((temp >> 2) & 0x01)
 temp_internal = (((temp >> 4) & 0x7FF) * 0.0625) * ((-1) * ((temp >> 15) & 0x01))
+temp_thermocouple = (((temp >> 18) & 0x3FFF) * 0.25) * ((-1) * ((temp >> 31) & 0x01))
 
 spi.close
 
@@ -17,3 +18,4 @@ print("Error open connection: " + str(error_open_connection))
 print("Error short circuit to GND: " + str(error_short_circuit_GND))
 print("Error short circuit to VCC: " + str(error_short_circuit_VCC))
 print("Internal junction temperature: " + str(temp_internal) + " °C")
+print("Thermocouple temperature: " + str(temp_thermocouple) + " °C")
