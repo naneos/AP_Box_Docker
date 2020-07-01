3a169325449f47bc98714d36ec0734d4b38779b6
Pelegorn <m.huegi@outlook.com>
2020-07-01 10:13:53 +0200

    temp  detection works properly

--- gpio/spi.py
+++ gpio/spi.py
@@ -4,21 +4,21 @@ def readTemp():
     spi = spidev.SpiDev()
     spi.open(0,0) # opens CS0
     spi.max_speed_hz = 3900000 #3.9MHz
-
     temp_bytes = spi.readbytes(4)
+    spi.close
 
     temp = int.from_bytes(temp_bytes, byteorder='big')
     error_open_connection = bool(temp & 0x01)
     error_short_circuit_GND = bool((temp >> 1) & 0x01)
     error_short_circuit_VCC = bool((temp >> 2) & 0x01)
-    temp_internal = (((temp >> 3) & 0x7FF) * 0.0625) * ((-1)**((temp >> 14) & 0x01))
-    #temp_thermocouple = (((temp >> 17) & 0x3FFF) * 0.25) * ((-1)**((temp >> 30) & 0x01))
-    if((temp >> 30) & 0x01):
-        temp_thermocouple = ((8191 - ((temp >> 17) & 0x3FFF)) * 0.25)
+    if((temp >> 15) & 0x01):
+        temp_internal = (2047 - ((temp >> 4) & 0x7FF)) * 0.0625
     else:
-        temp_thermocouple = (((temp >> 17) & 0x3FFF) * 0.25)
-
-    spi.close
+        temp_internal = ((temp >> 4) & 0x7FF) * 0.0625
+    if((temp >> 31) & 0x01):
+        temp_thermocouple = (8191 - ((temp >> 18) & 0x3FFF)) * 0.25
+    else:
+        temp_thermocouple = ((temp >> 18) & 0x3FFF) * 0.25
 
     print("Error open connection: " + str(error_open_connection))
     print("Error short circuit to GND: " + str(error_short_circuit_GND))
